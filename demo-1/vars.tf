98e40c019c2026c95bad78a002ed04b069d1ea5c
Ben Boskin <b.boskin96@gmail.com>
2022-02-21 13:10:02 -0800

    first push, updating region and local tfvars file

--- demo-1/vars.tf
+++ demo-1/vars.tf
@@ -5,13 +5,14 @@ variable "AWS_SECRET_KEY" {
 }
 
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "us-west-1"
 }
 
 variable "AMIS" {
   type = map(string)
   default = {
     us-east-1 = "ami-13be557e"
+    us-west-1 = "ami-0fc2b82bda0559ee0"
     us-west-2 = "ami-06b94666"
     eu-west-1 = "ami-0d729a60"
   }
